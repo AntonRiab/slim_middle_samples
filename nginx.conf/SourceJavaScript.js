1f3e3594d1888748d7abc6eeaf3e7ae35fdbc17e
AntonRiab <riab765@gmail.com>
2017-06-20 15:03:06 +0700

    More minimal for js

--- nginx.conf/SourceJavaScript.js
+++ nginx.conf/SourceJavaScript.js
@@ -28,16 +28,11 @@ function BadFilter(req) {
  */
     var v0, full_filter = "";
     for (v0 in req.args) {
-        if (full_filter.length > 1) {
-            full_filter += " AND";
-        }
+        if (full_filter.length > 1) full_filter += " AND";
 
-        if(v0 != 'undefined') {
+        if(v0 != 'undefined')
             full_filter += " " + v0 + "='" + req.args[v0] + "'";
-        }
     }
 
-    if (full_filter.length > 0) {
-        return " WHERE"+full_filter+"\n";
-    }
+    if (full_filter.length > 0) return " WHERE"+full_filter+"\n";
 }
